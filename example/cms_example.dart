5e3891aadb74f2707a64afc3af8e8f5bcecb2135
Dz <MathBeWithCode@gmail.com>
2025-08-23 11:33:00 +0800

    base feature

--- example/cms_example.dart
+++ example/cms_example.dart
@@ -1,6 +1,6 @@
-import 'package:cms/cms.dart';
+// import 'package:cms/cms.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+// void main() {
+//   var awesome = Awesome();
+//   print('awesome: ${awesome.isAwesome}');
+// }
--- lib/src/cms_base.dart
+++ lib/src/cms_base.dart
@@ -1,6 +1,44 @@
-// TODO: Put public facing types in this file.
+import 'dart:async';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'package:bloc/bloc.dart';
+
+typedef Relay<A> =
+    Future<void> Function<T>(Stream<T> stream, A Function(T) onData);
+
+/// Controlled Markov Stream
+abstract class Cms<S, A> extends Bloc<A, S> {
+  Cms(super.s0, [EventTransformer<A>? transformer]) {
+    on<A>(_defaultHandler, transformer: transformer);
+  }
+
+  /// markov kernel
+  FutureOr<S?> kernel(S s, A a, void Function(A) dispatch, Relay<A> relay);
+
+  Future<void> _handler(A event, Emitter<S> emit) async {
+    final s = await kernel(
+      state,
+      event,
+      add,
+      <T>(stream, onData) =>
+          emit.onEach<T>(stream, onData: (data) => add(onData(data))),
+    );
+    if (s != null) {
+      emit(s);
+    }
+  }
+
+  Future<void> _defaultHandler(A event, Emitter<S> emit) async {
+    if (_paceGuards.any((t) => t(event))) {
+      return;
+    }
+    return _handler(event, emit);
+  }
+
+  final List<bool Function(A)> _paceGuards = [];
+
+  void pace<E extends A>(EventTransformer<E> transformer) {
+    _paceGuards.add((event) => event is E);
+
+    on<E>(_handler, transformer: transformer);
+  }
 }
