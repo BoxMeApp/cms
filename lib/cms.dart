a127555f163f48430fe275dbc6f0e7a9475bf108
Dz <MathBeWithCode@gmail.com>
2025-11-23 18:07:31 +0800

    fix

--- lib/cms.dart
+++ lib/cms.dart
@@ -1,9 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
 export 'src/cms_base.dart';
 export 'src/error.dart';
 export 'package:bloc/bloc.dart';
-// TODO: Export any libraries intended for clients of this package.
